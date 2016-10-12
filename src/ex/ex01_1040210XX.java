e313b615ba20cc3bdf2bbf724abcb4eac70b08e0
ricky80022 <105021088@live.asia.edu.tw>
2016-10-12 11:57:00 +0800

    ex01

--- src/ex/ex01_1040210XX.java
+++ src/ex/ex01_1040210XX.java
@@ -5,11 +5,25 @@
  * Author: 1040210XX 周永振老師
  */
 
+import java.util.Scanner;
+
 public class ex01_1040210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scn = new Scanner(System.in);
+		int a, b;
+		System.out.print("請輸入 a 與 b 的值: ");
+		a = scn.nextInt();
+		b = scn.nextInt();
+		int i = a, sum = 0;
+		while(i <= b){
+			sum += i;
+			i++;
+		}
+		System.out.print("從 " + a + " 加到 " + b + " 的總合為 " + sum);
 	}
 
 }
+
